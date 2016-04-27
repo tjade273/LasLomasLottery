68ddb9a158ab9e7ca4c7c3d7914f63316ee1b66f
tjade273 <tjade273@gmail.com>
2016-04-27 12:12:04 -0700

    Add lightwallet support

--- truffle.js
+++ truffle.js
@@ -2,8 +2,10 @@ module.exports = {
   build: {
     "index.html": "index.html",
     "app.js": [
-      "javascripts/app.js"
+      "javascripts/app.js",
+      "javascripts/lightwallet.min.js"
     ],
+
     "app.css": [
       "stylesheets/app.css"
     ],
